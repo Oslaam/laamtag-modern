b7c9b830a2351b0459b3290bd1b8d28ca93cedcd
oslaam <oslaam1998@gmail.com>
2026-01-17 15:28:42 +0100

    fix: serve manifest as json

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-
-    async rewrites() {
-        return [
-            {
-                source: '/api/rpc-proxy',
-                destination: 'https://mainnet.helius-rpc.com/?api-key=a2488320-5767-4074-8bfe-8eda86de12f3',
-            },
-        ];
-    },
-
     async headers() {
         return [
             {
@@ -22,16 +12,16 @@ const nextConfig = {
                     },
                 ],
             },
+        ];
+    },
+    async rewrites() {
+        return [
             {
-                source: '/api/:path*',
-                headers: [
-                    { key: 'Access-Control-Allow-Origin', value: '*' },
-                    { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type, solana-client' },
-                ],
+                source: '/api/rpc-proxy',
+                destination: 'https://mainnet.helius-rpc.com/?api-key=a2488320-5767-4074-8bfe-8eda86de12f3',
             },
         ];
     },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
