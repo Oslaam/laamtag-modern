27eb9499221c8b0736373dcd1235eea97bbb374c
oslaam <oslaam1998@gmail.com>
2026-01-17 12:11:30 +0100

    Web APP to APK Preparation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -230,7 +230,18 @@ const App: FC<AppProps> = ({ Component, pageProps }) => (
     <ContextProvider>
         <Head>
             <title>LaamTag - Terminal</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+            {/* 1. Mobile specific viewport settings */}
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover" />
+            
+            {/* 2. Link to your manifest file */}
+            <link rel="manifest" href="/manifest.json" />
+            
+            {/* 3. Theme color for the Android status bar */}
+            <meta name="theme-color" content="#eab308" />
+            
+            {/* 4. iOS support (optional but good) */}
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         </Head>
         <GlobalLayout>
             <Component {...pageProps} />
