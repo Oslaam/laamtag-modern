fb6b60d2cb6e9d551bb5251ba66deeae0771334d
oslaam <oslaam1998@gmail.com>
2026-02-20 10:21:02 +0100

    Game Chances Updated

--- src/pages/api/games/spin.ts
+++ src/pages/api/games/spin.ts
@@ -16,15 +16,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
 
         const rewards = [
-            { label: "1 TAG", type: "TAG", value: 1, chance: 20 },
+            { label: "1 TAG", type: "TAG", value: 1, chance: 19.85 },
             { label: "5 TAG", type: "TAG", value: 5, chance: 13 },
             { label: "50 L", type: "LAAM", value: 50, chance: 30 },
             { label: "100 L", type: "LAAM", value: 100, chance: 10.3 },
             { label: "500 L", type: "LAAM", value: 500, chance: 13.7 },
-            { label: "1 USDC", type: "USDC", value: 1, chance: 0 },
-            { label: "0.01 SOL", type: "SOL", value: 0.01, chance: 0 },
+            { label: "1 USDC", type: "USDC", value: 1, chance: 0.05 },
+            { label: "0.01 SOL", type: "SOL", value: 0.01, chance: 0.05 },
             { label: "EMPTY", type: "EMPTY", value: 0, chance: 10 },
-            { label: "GEN BOX", type: "GEN_BOX", value: 0, chance: 0 },
+            { label: "GEN BOX", type: "GEN_BOX", value: 0, chance: 0.05 },
             {
                 label: "SPEC BOX",
                 type: "SPEC_BOX",
